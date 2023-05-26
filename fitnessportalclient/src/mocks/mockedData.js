082ec8e4e9c041d36589a2d1a2bbe1ada564b01c
MichalZdanuk <zdanuk_m@wp.pl>
2023-05-26 19:42:46 +0200

    implemented friendsPage view

---
+++ fitnessportalclient/src/mocks/mockedData.js
@@ -0,0 +1,10 @@
+export const friendsList = [
+  { id: 1, username: "fitguru123", email: "fitguru123@example.com", linkToProfile: "" },
+  { id: 2, username: "gymlover456", email: "gymlover456@example.com", linkToProfile: "" },
+  { id: 3, username: "healthnut789", email: "healthnut789@example.com", linkToProfile: "" },
+  { id: 4, username: "activelife123", email: "activelife123@example.com", linkToProfile: "" },
+  { id: 5, username: "workoutqueen456", email: "workoutqueen456@example.com", linkToProfile: "" },
+  { id: 6, username: "fitnessfanatic789", email: "fitnessfanatic789@example.com", linkToProfile: "" },
+  { id: 7, username: "exerciseenthusiast123", email: "exerciseenthusiast123@example.com", linkToProfile: "" },
+  { id: 8, username: "wellnesswarrior456", email: "wellnesswarrior456@example.com", linkToProfile: "" },
+];
--- fitnessportalclient/src/pages/FriendsPage.js
+++ fitnessportalclient/src/pages/FriendsPage.js
@@ -1,11 +1,54 @@
+import classes from "./FriendsPage.module.css";
+import friendsImg from "../assets/images/friends.png";
+import addFriendIcon from "../assets/images/add-user.png";
+import userIcon from "../assets/images/user.png";
+import {friendsList} from "../mocks/mockedData"
 
 const FriendsPage = () => {
-    return (
-        <>
-        <h2>Friends page</h2>
-        <p>To be implemented</p>
-        </>
-    );
+  return (
+    <div className={classes["container"]}>
+      <div className={classes["left-side"]}>
+        <img
+          src={friendsImg}
+          className={classes["friends-img"]}
+          alt="friends"
+        />
+        <div className={classes["input-container"]}>
+          <label className={classes["add-friend-label"]}>Friends email</label>
+          <input
+            placeholder="friend@wp.pl"
+            className={classes["input-box"]}
+          ></input>
+        </div>
+        <button className={classes["add-friend-button"]}>
+          ADD FRIEND{" "}
+          <img src={addFriendIcon} className={classes["add-img"]} alt="add" />
+        </button>
+      </div>
+
+      <div className={classes["right-side"]}>
+        <p className={classes["list-of-friends-label"]}>List of friends</p>
+        <FriendsList friendsData={friendsList} />
+        <button className={classes["show-more-button"]}>Show more</button>
+      </div>
+    </div>
+  );
 };
+
+const FriendsList = (props) => {
+    const listOfFriends = props.friendsData.map((friend) => {
+      return (
+        <div className={classes["friend-row"]}>
+          <div className={classes["friend-info"]}>
+            <img src={userIcon} className={classes["user-img"]} alt="user" />
+            <p className={classes["friend-elem"]}><emph className={classes["friend-username"]}>{friend.username}</emph> {friend.email}</p>
+          </div>
+          <button className={classes["profile-button"]}>Profile</button>
+        </div>
+      );
+    });
+  
+    return <div>{listOfFriends}</div>;
+  };
 
 export default FriendsPage;
