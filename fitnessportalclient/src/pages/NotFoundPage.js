0646316b60f59f06c33b8fbe200f5e09c9d90cfd
MichalZdanuk <zdanuk_m@wp.pl>
2023-06-06 10:42:18 +0200

    implemented pageNotFound view

--- fitnessportalclient/src/pages/NotFoundPage.js
+++ fitnessportalclient/src/pages/NotFoundPage.js
@@ -1,12 +1,24 @@
-import classes from "./NotFoundPage.module.css"
+import classes from "./NotFoundPage.module.css";
+import pageNotFoundIcon from "../assets/images/pageNotFound.png"
+import { useNavigate } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 const NotFoundPage = () => {
-    return (
-        <div className={classes["not-found"]}>
-            <h3>Page not found</h3>
-            <p className={classes["red"]}>to be implemented</p>
+    const navigate = useNavigate();
+  return (
+    <div className={classes["container"]}>
+      <div className={classes["notFound"]}>
+        <img className={classes["img"]} src={pageNotFoundIcon} alt="not found" />
+        <div className={classes["textBlock"]}>
+        <p className={classes["pageNotFound"]}>PAGE NOT FOUND</p>
+        <p className={classes["textRow"]}>We looked everywhere for this page.<br/>
+        Are you sure the website URL is correct?<br/>
+        Get in touch with the site owener.</p>
+        <Button onClick={() => {navigate("/")}} variant="outline-info" size="lg">Go Back Home</Button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default NotFoundPage;
