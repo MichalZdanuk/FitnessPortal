8c53171cd9acdabfee8b289ea45da60d803d3984
MichalZdanuk <zdanuk_m@wp.pl>
2023-06-28 03:07:58 +0200

    added animations to MainPage

--- fitnessportalclient/src/components/mainPage/MainPageContent.js
+++ fitnessportalclient/src/components/mainPage/MainPageContent.js
@@ -8,8 +8,8 @@ import book from "../../assets/images/book.png";
 import two_dumbbells from "../../assets/images/two_dumbbells.png";
 import friends from "../../assets/images/two_friends.png";
 import progress from "../../assets/images/progress.png";
-
-
+import AnimatedElement from "../animations/AnimatedElement";
+import NumberCounter from 'number-counter';
 
 const MainPageContent = () => {
     const navigate = useNavigate();
@@ -20,12 +20,28 @@ const MainPageContent = () => {
         <div className={classes["div-container"]}>
             <div className={classes["motto-div"]}>
                 <div className={classes["motto-text-div"]}>
+                <AnimatedElement animationType="slide-in" base="animated-text">
                     <h1 className={classes["motto-header"]}><span className={classes["text-with-border"]}>SHAPE</span> YOUR<br/> IDEAL BODY</h1>
                     <p className={classes["motto-text"]}><span className={classes["green-text"]}>HealthyHabitHub</span> will help you to measure your progress and achieve your dream body.</p>
                     {isUserLogged === false && <button className={classes["start-button"]} onClick={() => navigate("/register")}>START NOW</button>}
+                </AnimatedElement>
                 </div>
                 <div className={classes["motto-img-div"]}>
                     <img src={workout} className={classes["workout-img"]} alt=""/>
+                </div>
+            </div>
+            <div className={classes["statistics-div"]}>
+                <div className={classes["statistic"]}>
+                    <span className={classes["numbers"]}><NumberCounter end={56} start={30} delay='4' preFix="+"/></span>
+                    <p className={classes["statistic-text"]}>ARTICLES</p>
+                </div>
+                <div className={classes["statistic"]}>
+                <span className={classes["numbers"]}><NumberCounter end={978} start={800} delay='4' preFix="+"/></span>
+                    <p className={classes["statistic-text"]}>MEMBERS JOINED</p>
+                </div>
+                <div className={classes["statistic"]}>
+                <span className={classes["numbers"]}><NumberCounter end={12} start={1}/></span>
+                    <p className={classes["statistic-text"]}>EXERCISES</p>
                 </div>
             </div>
             <div className={classes["advantages-div"]}>
@@ -34,49 +50,11 @@ const MainPageContent = () => {
                     <BenefitList benefits={benefits} />
                 </div>
             </div>
-            <div className={classes["statistics-div"]}>
-                <div className={classes["statistic"]}>
-                    <span>+ 56</span>
-                    <p className={classes["statistic-text"]}>ARTICLES</p>
-                </div>
-                <div className={classes["statistic"]}>
-                    <span>+ 25 k</span>
-                    <p className={classes["statistic-text"]}>MEMBERS JOINED</p>
-                </div>
-                <div className={classes["statistic"]}>
-                    <span>12</span>
-                    <p className={classes["statistic-text"]}>EXERCISES</p>
-                </div>
-            </div>
             <div className={classes["offer-div"]}>
-                <div className={classes["offer"]}>
-                    <img src={book} className={classes["offer-img"]} alt=""/>
-                    <h3 className={classes["offer-heading"]}>ARTICLES</h3>
-                    <p className={classes["offer-text"]}>
-                        Gain valuable knowledge about diet, regeneration, exercises, performance, and more through our extensive collection of articles on the fitness portal.
-                    </p>
-                </div>
-                <div className={classes["offer"]}>
-                    <img src={two_dumbbells} className={classes["offer-img"]} alt=""/>
-                    <h3 className={classes["offer-heading"]}>EXERCISES</h3>
-                    <p className={classes["offer-text"]}>
-                        Explore our comprehensive exercises lexicon, offering concise instructions and guidance on how to perform various exercises effectively.
-                    </p>
-                </div>
-                <div className={classes["offer"]}>
-                    <img src={friends} className={classes["offer-img"]} alt=""/>
-                    <h3 className={classes["offer-heading"]}>FRIENDS</h3>
-                    <p className={classes["offer-text"]}>
-                        Connect with friends, track their results, and compare profiles to stay motivated and inspired on your fitness journey.   
-                    </p>
-                </div>
-                <div className={classes["offer"]}>
-                    <img src={progress} className={classes["offer-img"]} alt=""/>
-                    <h3 className={classes["offer-heading"]}>PROGRESS</h3>
-                    <p className={classes["offer-text"]}>
-                        Monitor your progress, analyze workout data, and visualize your history through interactive diagrams, empowering you to achieve continuous improvement and growth.   
-                    </p>
-                </div>
+                <Offer img={book} name="ARTICLES" content="Gain valuable knowledge about diet, regeneration, exercises, performance, and more through our extensive collection of articles on the fitness portal." />
+                <Offer img={two_dumbbells} name="EXERCISES" content="Explore our comprehensive exercises lexicon, offering concise instructions and guidance on how to perform various exercises effectively." />
+                <Offer img={friends} name="FRIENDS" content="Connect with friends, track their results, and compare profiles to stay motivated and inspired on your fitness journey." />
+                <Offer img={progress} name="PROGRESS" content="Monitor your progress, analyze workout data, and visualize your history through interactive diagrams, empowering you to achieve improvement and growth." />
             </div>
         </div>
     )
@@ -84,10 +62,12 @@ const MainPageContent = () => {
 
 const Benefit = (props) => {
     return(
-        <div className={classes["advantage-div"]}>
-            <img src={props.img} className={classes["benefit-icon"]} alt="a"/>
-            <p className={classes["benefit-text"]}>{props.content}</p>
-        </div>
+        <AnimatedElement animationType="grow" base="animated-v2">
+            <div className={classes["advantage-div"]}>
+                <img src={props.img} className={classes["benefit-icon"]} alt="a"/>
+                <p className={classes["benefit-text"]}>{props.content}</p>
+            </div>
+        </AnimatedElement>
     );
 };
 
@@ -95,6 +75,18 @@ const BenefitList = (props) => {
     const benefits = props.benefits.map((b) => {return <Benefit key={b.id} img={b.img} content={b.content}/>})
     return (
         <>{benefits}</>
+    );
+};
+
+const Offer = (props) => {
+    return (
+        <AnimatedElement animationType="grow-tiny" base="animated-v2">
+            <div className={classes["offer"]}>
+                <img src={props.img} className={classes["offer-img"]} alt=""/>
+                <h3 className={classes["offer-heading"]}>{props.name}</h3>
+                <p className={classes["offer-text"]}>{props.content}</p>
+            </div>
+        </AnimatedElement>
     );
 };
 
