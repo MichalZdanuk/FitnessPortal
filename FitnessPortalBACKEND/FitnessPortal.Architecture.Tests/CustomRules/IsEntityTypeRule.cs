8f846d74392d8174b1e0c5307b1ba05558f164c7
MichalZdanuk <zdanuk_m@wp.pl>
2025-04-11 23:44:09 +0200

    Introduct basic rules and global usings.

---
+++ FitnessPortalBACKEND/FitnessPortal.Architecture.Tests/CustomRules/IsEntityTypeRule.cs
@@ -0,0 +1,22 @@
+﻿using Mono.Cecil;
+using NetArchTest.Rules;
+
+namespace FitnessPortal.Architecture.Tests.CustomRules;
+public class IsEntityTypeRule
+	: ICustomRule
+{
+	public bool MeetsRule(TypeDefinition type)
+	{
+		if (!type.IsClass)
+		{
+			return false;
+		}
+
+		if (!type.IsPublic)
+		{
+			return false;
+		}
+
+		return true;
+	}
+}
---
+++ FitnessPortalBACKEND/FitnessPortal.Architecture.Tests/CustomRules/IsEnumTypeRule.cs
@@ -0,0 +1,29 @@
+﻿using Mono.Cecil;
+using NetArchTest.Rules;
+
+namespace FitnessPortal.Architecture.Tests.CustomRules;
+public class IsEnumTypeRule
+	: ICustomRule
+{
+	private const string _enumSuffix = "Enum";
+
+	public bool MeetsRule(TypeDefinition type)
+	{
+		if (!type.IsEnum)
+		{
+			return false;
+		}
+
+		if (!HasValidName(type))
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+	private bool HasValidName(TypeDefinition type)
+	{
+		return type.Name.EndsWith(_enumSuffix, StringComparison.Ordinal);
+	}
+}
