ea7e61c59f84f1fc7afbae94697dafda10e8a917
MichalZdanuk <zdanuk_m@wp.pl>
2025-04-12 00:04:52 +0200

    Extract namespaces to consts, add enum localization rule.

---
+++ FitnessPortalBACKEND/FitnessPortal.Architecture.Tests/Consts.cs
@@ -0,0 +1,9 @@
+﻿namespace FitnessPortal.Architecture.Tests;
+public class Consts
+{
+	public class Namespaces
+	{
+		public const string EntitiesNamespace = "FitnessPortalAPI.Entities";
+		public const string EnumsNamespace = "FitnessPortalAPI.Enums";
+	}
+}
--- FitnessPortalBACKEND/FitnessPortal.Architecture.Tests/Tests/CoreTests/CoreTests.cs
+++ FitnessPortalBACKEND/FitnessPortal.Architecture.Tests/Tests/CoreTests/CoreTests.cs
@@ -5,8 +5,6 @@ namespace FitnessPortal.Architecture.Tests.Tests.CoreTests;
 [TestClass]
 public class CoreTests
 {
-	private const string EntitiesNamespace = "FitnessPortalAPI.Entities";
-	private const string EnumsNamespace = "FitnessPortalAPI.Enums";
 	private Assembly _assembly = typeof(FitnessPortalAPI.AssemblyMarker).Assembly;
 
 	[TestMethod]
@@ -18,7 +16,7 @@ public class CoreTests
 		// Act
 		var result = Types.InAssembly(_assembly)
 			.That()
-			.ResideInNamespace(EntitiesNamespace)
+			.ResideInNamespace(Consts.Namespaces.EntitiesNamespace)
 			.Should()
 			.MeetCustomRule(isEntityTypeRule)
 			.GetResult();
@@ -40,7 +38,7 @@ public class CoreTests
 		// Act
 		var result = Types.InAssembly(_assembly)
 			.That()
-			.ResideInNamespace(EnumsNamespace)
+			.ResideInNamespace(Consts.Namespaces.EnumsNamespace)
 			.Should()
 			.MeetCustomRule(isEnumTypeRule)
 			.GetResult();
@@ -50,6 +48,28 @@ public class CoreTests
 		{
 			var failingTypes = string.Join(", ", result.FailingTypes.Select(_t => _t.Name));
 			Assert.Fail($"The following types do not meet the Enum naming and type conventions: {failingTypes}");
+		}
+	}
+
+	[TestMethod]
+	public void Enums_Should_Only_Exist_In_Enums_Namespace()
+	{
+		// Arrange
+		var isEnumTypeRule = new IsEnumTypeRule();
+
+		// Act
+		var result = Types.InAssembly(_assembly)
+			.That()
+			.MeetCustomRule(isEnumTypeRule)
+			.Should()
+			.ResideInNamespace(Consts.Namespaces.EnumsNamespace)
+			.GetResult();
+
+		// Assert
+		if (!result.IsSuccessful)
+		{
+			var failingTypes = string.Join(", ", result.FailingTypes.Select(t => $"{t.Name} ({t.Namespace})"));
+			Assert.Fail($"The following Enums are not in '{Consts.Namespaces.EnumsNamespace}' actual namespaces: {failingTypes}");
 		}
 	}
 
