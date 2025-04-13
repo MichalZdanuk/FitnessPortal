7cc9f9094536d8600573fcac090ec27922cd18e8
MichalZdanuk <zdanuk_m@wp.pl>
2025-04-13 21:28:14 +0200

    Implement arch tests for middleware.

---
+++ FitnessPortalBACKEND/FitnessPortal.Architecture.Tests/CustomRules/IsMiddlewareTypeRule.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace FitnessPortal.Architecture.Tests.CustomRules;
+public class IsMiddlewareTypeRule
+	: ICustomRule
+{
+	public bool MeetsRule(TypeDefinition type)
+	{
+		if (!type.IsPublicClass())
+		{
+			return false;
+		}
+
+		if (!ImplementsIMiddleware(type))
+		{
+			return false;
+		}
+
+		if (!type.HasValidSuffix(Consts.Sufixes.MiddlewareSuffix))
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+	private bool ImplementsIMiddleware(TypeDefinition type)
+	{
+		return type.Interfaces
+			.Any(i => i.InterfaceType.FullName == Consts.Sufixes.MiddlewareFullName);
+	}
+}
